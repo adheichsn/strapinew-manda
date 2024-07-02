9c154722a78fdbf587b8b887379947ed4428265c
Amanda1905 <amandaputri.ap23@gmail.com>
2024-07-02 16:25:16 +0700

    ocean

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,11 +1,16 @@
 module.exports = ({ env }) => ({
     connection: {
-        client: "postgres",
-        connection: {
-            host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-            database: env("DATABASE_NAME"),
-            user: env("DATABASE_USER"),
-            password: env("DATABASE_PASSWORD"),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
         },
+      },
+      debug: false,
     },
-});
+  });
